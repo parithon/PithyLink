a07915dd598a098d08720c6a216ab9fcde34f3e0
Anthony Conrad <8602418+parithon@users.noreply.github.com>
2018-07-01 17:28:49 -0700

    Removed boilerplate files and created Index layout.

---
+++ PithyLink.Web/Attributes/IsValidUrlAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PithyLink.Web.Attributes
+{
+    internal class IsValidUrlAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            return value is string == false
+                ? new ValidationResult($"Unknown property: {value}")
+                : !Uri.TryCreate(value as string, UriKind.Absolute, out Uri result)
+                ? new ValidationResult("Not a vadid url.")
+                : result.Scheme != "http" && result.Scheme != "https"
+                ? new ValidationResult("Only a valid HTTP or HTTPS url is supported at this time.")
+                : ValidationResult.Success;
+        }
+    }
+}
--- PithyLink.Web/Pages/Index.cshtml.cs
+++ PithyLink.Web/Pages/Index.cshtml.cs
@@ -1,17 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PithyLink.Web.Attributes;
 
 namespace PithyLink.Web.Pages
 {
     public class IndexModel : PageModel
     {
+
+        [Required]
+        [IsValidUrl]
+        [BindProperty]
+        [Display(Name = "Url")]
+        public string ShortenUrl { get; set; }
+
         public void OnGet()
         {
 
+        }
+
+        public void OnPost()
+        {
+            if (!ModelState.IsValid)
+                return;
+
+            RedirectToPage("/Index");
         }
     }
 }
